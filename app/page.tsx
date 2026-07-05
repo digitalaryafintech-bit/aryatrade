e3f505b9ec8102a95a69e03a3aeb88c296a9f66f
digitalaryafintech-bit <digitalaryafintech@gmail.com>
2026-07-05 12:35:48 +0530

    Update and rename app/page.tsx to index.html

--- app/page.tsx
+++
@@ -1,8 +0,0 @@
-export default function Home() {
-  return (
-    <div style={{ padding: '40px', textAlign: 'center', fontFamily: 'sans-serif' }}>
-      <h1>Arya Trade Live!</h1>
-      <p>Aapka project successfully deploy ho gaya hai bhai.</p>
-    </div>
-  );
-}
